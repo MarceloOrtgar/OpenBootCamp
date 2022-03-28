a403bfb6d96b37f7f057b2e12b86c16e6bc0e58e
Marcelo Ortega <marceloortega18@gmail.com>
2022-03-27 20:00:58 -0400

    Primera parte

---
+++ src/com/company/Problema1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Problema1 {
+
+    public static void main(String[] args) {
+
+        int res=sumaNumeros(5,6,20);
+
+        System.out.println(res);
+
+    }
+
+    public static int sumaNumeros(int num1, int num2, int num3){
+        int resultado=num1+num2+num3;
+        return resultado;
+    }
+
+
+
+}
